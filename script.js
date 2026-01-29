2bf13dfa3256f7ffc123392ed4b762764a1381a8
FootballAnalystAI.github.io <avishkarmenge703@gmail.com>
2026-01-29 12:04:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Select All Buttons
+document.querySelectorAll("button").forEach(btn => {
+
+  btn.addEventListener("click", () => {
+    alert("✅ Player Added to Team11 AI Suggestions!");
+  });
+
+});
